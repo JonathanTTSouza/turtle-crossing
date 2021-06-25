7b7a00d9100c259bebd4247e61ecdfb0dc5ebdcf
JonathanTTSouza <68395424+JonathanTTSouza@users.noreply.github.com>
2021-06-24 22:29:55 -0300

    Comments

--- main.py
+++ main.py
@@ -19,16 +19,19 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
-    scoreboard.write_scoreboard(player.win_count)
+    scoreboard.write_scoreboard(player.win_count)  # Level number display
 
+    # Player movement
     screen.onkey(player.move, "Up")
 
+    # Car spawn
     if game_count % 6 == 0:
         new_car = CarManager()
         car_list.append(new_car)
     for car in car_list:
         car.car_move(player.win_count)
 
+        # Player collision with car
         if car.distance(player) < 17:
             scoreboard.game_over()
             game_is_on = False
